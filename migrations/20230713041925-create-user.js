0b4faff6b68a1fae6f2eaa0e0a197bfb7d07462b
CHUCHUDAN <daniel870217@gmail.com>
2023-07-13 13:15:29 +0800

    fix: 20230713041925-create-user.js

--- migrations/20230713041925-create-user.js
+++ migrations/20230713041925-create-user.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       password: {
         allowNull: false,
-        type: Sequelize.STRING(50)
+        type: Sequelize.STRING
       },
       role: {
         allowNull: false,
