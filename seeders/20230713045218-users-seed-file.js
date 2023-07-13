31d0be1c943f363532601d9b1660c2f60312871e
CHUCHUDAN <daniel870217@gmail.com>
2023-07-13 13:17:19 +0800

    seed: add 20230713045218-users-seed-file.js

---
+++ seeders/20230713045218-users-seed-file.js
@@ -0,0 +1,26 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [{
+      name: 'buyer',
+      account: 'buyer001',
+      password: await bcrypt.hash('titaner', 10),
+      role: 'buyer',
+      created_at: faker.date.past(),
+      updated_at: faker.date.recent()
+    }, {
+      name: 'seller',
+      account: 'seller001',
+      password: await bcrypt.hash('titaner', 10),
+      role: 'seller',
+      created_at: faker.date.past(),
+      updated_at: faker.date.recent()
+    }], {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', {})
+  }
+}
