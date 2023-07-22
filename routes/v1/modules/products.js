e7e6ae8e8dbb3333b03fc681fc61ec9428a7386d
daniel <daniel870217@gmail.com>
2023-07-22 11:17:46 +0800

    feat: add getProduct

--- routes/v1/modules/products.js
+++ routes/v1/modules/products.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const productController = require('../../../controllers/product-controller')
 
+// 取得單一商品
+router.get('/:product_id', productController.getProduct)
+
 // 取得所有商品
 router.get('/', productController.getProducts)
 
