026133a5715d51386eb17337089a9477e448d9c8
daniel <daniel870217@gmail.com>
2023-07-23 17:10:29 +0800

    fix: fix getSeller and getStores

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user-controller')
+const storeController = require('../../controllers/store-controller')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 const passport = require('../../config/passport')
 const products = require('./modules/products')
@@ -15,6 +16,12 @@ const { validation } = require('../../middleware/validation')
 router.post('/users/signin', upload.single('avatar'), validation, passport.authenticate('local', { session: false }), userController.signIn)
 // 註冊
 router.post('/user', validation, userController.signUp)
+
+// 取得商家商品清單
+router.get('/stores/:seller_id', storeController.getStores)
+
+// 取得商家資料
+router.get('/users/:seller_id', userController.getSeller)
 
 router.use('/users', authenticated, users)
 router.use('/stores', authenticated, isSeller, stores)
